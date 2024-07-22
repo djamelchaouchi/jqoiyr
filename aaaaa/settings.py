1a2d1fdde189edc28f4a0aa53eda3f4dcb6b847a
unknown <jqoiyr@gmail.com>
2024-07-22 08:54:29 +0100

    first commit

--- aaaaa/settings.py
+++ aaaaa/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- aaaaa/urls.py
+++ aaaaa/urls.py
@@ -4,5 +4,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('accounts/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
+
+# handler404 = 'pages.views.error_404_view'  404 page
